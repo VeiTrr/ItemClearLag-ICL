69a3ec113ba6c474823958e8ea6076cf243eee44
Jim Zhou <csd_189@163.com>
2025-01-06 15:50:54 +0800

    Support for Fabric/1.21.4

--- src/main/java/vt/icl/ICL.java
+++ src/main/java/vt/icl/ICL.java
@@ -311,9 +311,9 @@ public class ICL implements ModInitializer {
         vec3d = new Vec3d(player.getX() + e / k * 2.0, player.getY() + f / k * 2.0, player.getZ() + g / k * 2.0);
         Identifier sound;
         if (isLastSound) {
-            sound = new Identifier(config.LastNotificationSound);
+            sound = Identifier.of(config.LastNotificationSound);
         } else {
-            sound = new Identifier(config.NotificationSound);
+            sound = Identifier.of(config.NotificationSound);
         }
         RegistryEntry<SoundEvent> registryEntry = RegistryEntry.of(SoundEvent.of(sound));
         player.networkHandler.sendPacket(new PlaySoundS2CPacket(registryEntry, SoundCategory.PLAYERS, vec3d.getX(), vec3d.getY(), vec3d.getZ(), 1, 1, 1));
